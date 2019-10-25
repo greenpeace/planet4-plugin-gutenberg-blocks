0cbe4ceabe9c37c97ae54655642801f420eea1cf
Emmanuel Aubrey <emmanuelthegrey@gmail.com>
2019-10-25 10:17:52 -0400

    js for slider

---
+++ public/js/slick.js
@@ -0,0 +1,56 @@
+jQuery(function ($) {
+    'use strict';
+
+$('.social-slides').slick({
+    dots: false,
+    arrows:true,
+    infinite: true,
+    speed: 300,
+    nextArrow: '<button class="slick-next"></button>',
+    prevArrow: '<button class="slick-prev"></button>',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          dots: false,
+          arrows:true,
+          infinite: true,
+          speed: 300,
+          nextArrow: '<button class="slick-next"></button>',
+          prevArrow: '<button class="slick-prev"></button>',
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+          arrows:true,
+          infinite: true,
+          speed: 300,
+          nextArrow: '<button class="slick-next"></button>',
+          prevArrow: '<button class="slick-prev"></button>',
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+          arrows:true,
+          infinite: true,
+          speed: 300,
+          nextArrow: '<button class="slick-next"></button>',
+          prevArrow: '<button class="slick-prev"></button>',
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+});
