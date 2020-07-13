3acd83cb4a12015451879128d16e21cd49994f02
Maud Leray <maudleray035@gmail.com>
2020-07-13 13:18:01 +0200

    PLANET-4811 Add more destructuring to SubmenuEditor

--- assets/src/blocks/Submenu/SubmenuEditor.js
+++ assets/src/blocks/Submenu/SubmenuEditor.js
@@ -16,38 +16,39 @@ export class SubmenuEditor extends Component {
   }
 
   renderEdit() {
-    const { attributes } = this.props;
+    const {
+      attributes,
+      onHeadingChange,
+      onLinkChange,
+      addLevel,
+      onStyleChange,
+      removeLevel
+    } = this.props;
 
     return (
       <Fragment>
         <InspectorControls>
           <PanelBody title={__('Setting', 'p4ge')}>
-            <h2>{__('Anchor Link Submenu', 'p4ge')}</h2>
-            <p><i>{__(
-              'An in-page table of contents to help users have a sense of what\'s on the page and let them jump to a topic they are interested in.',
-              'p4ge'
-            )}</i></p>
-            <hr />
             {attributes.levels.map((heading, i) => {
               return (
                 <MenuLevel
                   {...heading}
-                  onHeadingChange={this.props.onHeadingChange}
-                  onLinkChange={this.props.onLinkChange}
-                  onStyleChange={this.props.onStyleChange}
+                  onHeadingChange={onHeadingChange}
+                  onLinkChange={onLinkChange}
+                  onStyleChange={onStyleChange}
                   index={i}
                   key={i}
                 />
               );
             })}
             <Button isPrimary
-              onClick={this.props.addLevel}
+              onClick={addLevel}
               disabled={attributes.levels.length >= 3 || attributes.levels.slice(-1)[0].heading === 0}
             >
               Add level
           </Button>
             <Button isDefault
-              onClick={attributes.removeLevel} disabled={attributes.levels.length <= 1}
+              onClick={removeLevel} disabled={attributes.levels.length <= 1}
             >
               Remove level
           </Button>
