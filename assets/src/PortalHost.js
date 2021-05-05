383a14eecc5c10cc91b13cdb1cc011cbdd6cb288
inwerpsel <pieter.vincent1988@gmail.com>
2021-05-05 16:04:41 +0200

    Send dataLayer event when render is finished
    * To check if this resolves/reduces GTM blocking issues.
    * In order to "know" when all blocks are done, we need to render them in
    a single React element using portals, instead of doing separate renders.
    * Also emit event at start and finish of frontendIndex.js.

---
+++ assets/src/PortalHost.js
@@ -0,0 +1,46 @@
+import { Fragment, useEffect } from '@wordpress/element';
+import { SpreadsheetFrontend } from './blocks/Spreadsheet/SpreadsheetFrontend';
+import { CounterFrontend } from './blocks/Counter/CounterFrontend';
+import { ArticlesFrontend } from './blocks/Articles/ArticlesFrontend';
+import { CookiesFrontend } from './blocks/Cookies/CookiesFrontend';
+import { SplittwocolumnsFrontend } from './blocks/Splittwocolumns/SplittwocolumnsFrontend';
+import { HappypointFrontend } from './blocks/Happypoint/HappypointFrontend';
+import { GalleryFrontend } from './blocks/Gallery/GalleryFrontend';
+import { TimelineFrontend } from './blocks/Timeline/TimelineFrontend';
+import { SubmenuFrontend } from './blocks/Submenu/SubmenuFrontend';
+import { MediaFrontend } from './blocks/Media/MediaFrontend';
+import { ColumnsFrontend } from './blocks/Columns/ColumnsFrontend';
+
+// Render React components
+const COMPONENTS = {
+  'planet4-blocks/spreadsheet': SpreadsheetFrontend,
+  'planet4-blocks/counter': CounterFrontend,
+  'planet4-blocks/articles': ArticlesFrontend,
+  'planet4-blocks/cookies': CookiesFrontend,
+  'planet4-blocks/split-two-columns': SplittwocolumnsFrontend,
+  'planet4-blocks/happypoint': HappypointFrontend,
+  'planet4-blocks/gallery': GalleryFrontend,
+  'planet4-blocks/timeline': TimelineFrontend,
+  'planet4-blocks/submenu': SubmenuFrontend,
+  'planet4-blocks/media-video': MediaFrontend,
+  'planet4-blocks/columns': ColumnsFrontend,
+};
+
+export const PortalHost = ({blocks}) => {
+  useEffect(() => {
+    dataLayer in window && dataLayer.push({event: 'blocksRendered', time: performance.now()})
+  }, []);
+
+  return <Fragment>
+    {blocks.map(block=> {
+      const blockName = block.dataset.render;
+      const BlockFrontend = COMPONENTS[ blockName ];
+      if (!BlockFrontend) {
+        return null;
+      }
+      const attributes = JSON.parse( block.dataset.attributes );
+
+      return wp.element.createPortal(<BlockFrontend {...attributes.attributes } />, block);
+    })}
+  </Fragment>
+}
--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -1,43 +1,16 @@
-import { SpreadsheetFrontend } from './blocks/Spreadsheet/SpreadsheetFrontend';
-import { CounterFrontend } from './blocks/Counter/CounterFrontend';
-import { ArticlesFrontend } from './blocks/Articles/ArticlesFrontend';
-import { CookiesFrontend } from './blocks/Cookies/CookiesFrontend';
-import { SplittwocolumnsFrontend } from "./blocks/Splittwocolumns/SplittwocolumnsFrontend";
-import { HappypointFrontend } from './blocks/Happypoint/HappypointFrontend';
-import { GalleryFrontend } from './blocks/Gallery/GalleryFrontend';
-import { TimelineFrontend } from './blocks/Timeline/TimelineFrontend';
-import { SubmenuFrontend } from './blocks/Submenu/SubmenuFrontend';
-import { MediaFrontend } from './blocks/Media/MediaFrontend';
-import { ColumnsFrontend } from './blocks/Columns/ColumnsFrontend';
 import { setupMediaElementJS } from './blocks/Media/setupMediaElementJS';
 import { setupLightboxForImages } from './components/Lightbox/setupLightboxForImages';
+import { PortalHost } from './PortalHost';
 
+const dataLayer = window.dataLayer || [];
+
+dataLayer.push({ event: 'frontendIndexStart', time: performance.now()});
 // Render React components
-const COMPONENTS = {
-  'planet4-blocks/spreadsheet': SpreadsheetFrontend,
-  'planet4-blocks/counter': CounterFrontend,
-  'planet4-blocks/articles': ArticlesFrontend,
-  'planet4-blocks/cookies': CookiesFrontend,
-  'planet4-blocks/split-two-columns': SplittwocolumnsFrontend,
-  'planet4-blocks/happypoint': HappypointFrontend,
-  'planet4-blocks/gallery': GalleryFrontend,
-  'planet4-blocks/timeline': TimelineFrontend,
-  'planet4-blocks/submenu': SubmenuFrontend,
-  'planet4-blocks/media-video': MediaFrontend,
-  'planet4-blocks/columns': ColumnsFrontend,
-};
-
-document.querySelectorAll( `[data-render]` ).forEach(
-  blockNode => {
-    const blockName = blockNode.dataset.render;
-    const BlockFrontend = COMPONENTS[ blockName ];
-    if (!BlockFrontend) {
-      return;
-    }
-    const attributes = JSON.parse( blockNode.dataset.attributes );
-    wp.element.render( <BlockFrontend { ...attributes.attributes } />, blockNode );
-  }
-);
+// Empty react root so we can do a single render with portals to all the blocks.
+const reactRoot = document.createElement('div');
+const blocks = [...document.querySelectorAll( `[data-render]` )];
+wp.element.render(<PortalHost blocks={blocks} />, reactRoot);
 
 setupMediaElementJS();
 setupLightboxForImages();
+dataLayer.push({ event: 'frontendIndexDone', time: performance.now() });
