dcab3b2a12fe54768125ddee63f7c80c0dd4cc69
inwerpsel <pieter.vincent1988@gmail.com>
2022-01-17 12:45:53 +0100

    Bump theme editor to 2.0.8
    * Adds media query support and an open button to the WP admin menu.
    * Improved UI.

--- assets/src/themeEditorIndex.js
+++ assets/src/themeEditorIndex.js
@@ -1,8 +1,25 @@
 import { setupThemeEditor } from 'use-theme-editor/build/initializeThemeEditor';
 import { p4ServerThemes } from './theme/p4ServerThemes';
 
+const getLocalStorageNamespace = () => {
+  const base = `${document.documentElement.dataset.base}`;
+
+  if (!base) {
+    return '';
+  }
+
+  const [, rawNs] = base.split(window.location.origin);
+
+  if (!rawNs || rawNs === '/') {
+    return '';
+  }
+
+  return rawNs.replace(/^\//, '').replace(/\/*$/, '') + '/';
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
   setupThemeEditor({
     serverThemes: p4ServerThemes,
+    localStorageNamespace: getLocalStorageNamespace(),
   });
 });
