62fba0b2a2af819b673fb4ba9a4ae8610453ac5e
Dan Tovbein <dtovbein@greenpeace.org>
2022-01-21 08:09:03 -0300

    Revert "Feature/turn sharebuttons to a block"

--- assets/src/blocks/ShareButtons/ShareButtonsEditorScript.js
+++
@@ -1,3 +0,0 @@
-import { registerBlock } from './ShareButtonsBlock';
-
-registerBlock();
--- planet4-gutenberg-blocks.php
+++ planet4-gutenberg-blocks.php
@@ -144,7 +144,6 @@ const PAGE_BLOCK_TYPES = [
 const BETA_PAGE_BLOCK_TYPES = [
 	'planet4-blocks/enform-beta',
 	'planet4-blocks/hubspot-form',
-	'planet4-blocks/share-buttons',
 ];
 
 // campaigns allow all block types.
@@ -173,7 +172,6 @@ const BETA_CAMPAIGN_BLOCK_TYPES = [
 	'planet4-blocks/enform-beta',
 	'planet4-blocks/social-media-cards',
 	'planet4-blocks/hubspot-form',
-	'planet4-blocks/share-buttons',
 ];
 
 /**
