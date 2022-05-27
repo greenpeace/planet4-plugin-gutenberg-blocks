72b6199e73b9e958b57f8bdacb096e0068070090
Maud Leray <maudleray035@gmail.com>
2022-05-27 12:05:30 +0200

    Hide image credits when caption is empty
    This will be useful in block patterns in particular, where we don't want to show caption nor credits

--- assets/src/components/Image/ImageBlockEdit.js
+++ assets/src/components/Image/ImageBlockEdit.js
@@ -12,16 +12,20 @@ export const ImageBlockEdit = (BlockEdit) => {
     }
 
     const { attributes, clientId } = props;
-    const { id, caption } = attributes;
+    const { id, caption, className } = attributes;
 
     // Get image data
     const image = useSelect(select => id ? select('core').getMedia(id) : null);
     const credits = image?.meta?._credit_text;
     // Compile data for insertion
-    const image_credits = credits && credits.length > 0 && ! caption.includes(credits)
+    const image_credits = credits && credits.length > 0 && caption && ! caption.includes(credits)
       ? (credits.includes('©') ? credits : `© ${credits}`)
       : null;
     const block_id = clientId ? `block-${clientId}` : null;
+
+    if (!caption && credits && (!className || !className.includes('no-caption'))) {
+      attributes.className = className ? `${className} no-caption` : 'no-caption';
+    }
 
     return (
       <>
