c0f86ccd9846ce63a0199e0ad5fceb671bda36da
Maud Leray <maudleray035@gmail.com>
2024-06-24 11:53:14 +0200

    PLANET-7554 Remove Take Action block pattern layout from plugin
    We moved it to the master theme instead

--- assets/src/block-templates/template-list.js
+++ assets/src/block-templates/template-list.js
@@ -8,7 +8,6 @@ import * as highlightedCta from './highlighted-cta';
 import * as deepDiveTopic from './deep-dive-topic';
 import * as homepage from './homepage';
 import * as campaign from './campaign';
-import * as takeAction from './take-action';
 import * as action from './action';
 import * as getInformed from './get-informed';
 import * as highLevelTopic from './high-level-topic';
@@ -26,7 +25,6 @@ export default [
   deepDiveTopic,
   homepage,
   campaign,
-  takeAction,
   action,
   getInformed,
   highLevelTopic,
