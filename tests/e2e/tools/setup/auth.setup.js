74ef250afa0e48b53656b750ba59517497e96f5c
Nikos Roussos <nikos.roussos@greenpeace.org>
2024-11-12 08:50:44 +0200

    Remove e2e tests
    All of those tests have either been moved to the theme
    or the blocks have been retired (eg. S2C).
    
    Also removed related CI jobs.

--- tests/e2e/tools/setup/auth.setup.js
+++
@@ -1,6 +0,0 @@
-import {test as setup} from '../lib/test-utils';
-
-setup('authenticate', async ({page, requestUtils}) => {
-  await page.goto('./wp-admin/');
-  await requestUtils.setupRest();
-});
